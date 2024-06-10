2498d395d035991934b3833fa34c88920fc948a4
jeungsugi <yoouug72@gmail.com>
2024-06-10 16:06:55 +0900

    산책페이지 1차

--- src/components/diary/footer.tsx
+++ src/components/diary/footer.tsx
@@ -28,7 +28,7 @@ const MenuFooter = () => {
         nav(`/List`);
         break;
       case "산책":
-        nav(`/List`);
+        nav(`/walkPage`);
         break;
       case "커뮤":
         nav(`/communityList`);
