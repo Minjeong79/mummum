bf64f0f56164749307273c9a7de4aace79a2dc00
jeungsugi <yoouug72@gmail.com>
2024-06-14 14:35:50 +0900

    로그인 리듀서

--- src/components/login/logoutHeader.tsx
+++ src/components/login/logoutHeader.tsx
@@ -1,16 +1,21 @@
 import { useAppDispatch } from "../../redux/reduxStore";
 import { userLogout } from "../../redux/slices/loginSlice/userLoginSlice";
+import { useNavigate } from "react-router-dom";
 import supabase from "../../store";
 const Logout = () => {
   const dispatch = useAppDispatch();
+  const nav = useNavigate();
 
   const handleLogout = async () => {
     const popup = window.confirm("로그아웃 할래 멈?");
     if (popup) {
       const { error } = await supabase.auth.signOut();
+      nav(`/`);
       console.log(error);
+      
       localStorage.clear();
       dispatch(userLogout(""));
+     
     } else {
       return;
     }
